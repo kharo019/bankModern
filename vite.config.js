76b4108c3aa8adab6ea5019da9fb2db507f15c3a
kharo019 <kevinharo019@gmail.com>
2023-11-17 03:49:50 -0700

    Deployed website

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-  plugins: [react()],
-=======
   plugins: [react()]
->>>>>>> f19ecf9 ( Changes to be committed:)
 })
