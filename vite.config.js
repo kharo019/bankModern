1bcaa2ce3c34a43d9939f634985b9400fb98f3df
kharo019 <kevinharo019@gmail.com>
2023-11-17 03:36:26 -0700

    Changes to be committed: 	modified:   .gitignore 	modified:   dist/index.html 	deleted:    dist/vite.svg 	modified:   index.html 	modified:   package-lock.json 	modified:   package.json 	new file:   public/hoobank.svg 	deleted:    public/vite.svg 	modified:   src/main.jsx 	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+<<<<<<< HEAD
   plugins: [react()],
+=======
+  plugins: [react()]
+>>>>>>> f19ecf9 ( Changes to be committed:)
 })
