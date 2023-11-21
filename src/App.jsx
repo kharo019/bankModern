38ad39f3f1536995a0c7996760a43040bf93ac7c
Kevin Haro <kevinharo019@gmail.com>
2023-11-20 18:41:47 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,39 @@
-import styles from './style';
 
-import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer  } from './components';
+
+
+import styles from "./style";
+import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from "./components";
 
 const App = () => (
+  <>
+    <div className={`${styles.paddingX} ${styles.flexCenter} bg-primary w-full overflow-hidden sticky top-0 z-[9]`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
+    </div>
+    <div className="bg-primary w-full overflow-hidden">
 
-	<div className = "bg-primary w-full overflow-hidden">
-		<div className = {`${styles.paddingX} ${styles.flexCenter}`}>
-			<div className = {`${styles.boxWidth}`}>
-				<Navbar />
-			</div>
-		</div>
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
 
-		<div className = {`bg-primary ${styles.flexStart}`}>
-			<div className = {`${styles.boxWidth}`}>
-				<Hero /> 
-			</div>
-		</div>
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
+      </div>
 
-		<div className = {`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
-			<div className = {`${styles.boxWidth}`}>
-				<Stats />
-				<Business />
-				<Billing />
-				<CardDeal />
-				<Testimonials />
-				<Clients /> 
-				<CTA />
-				<Footer />  
-			</div>
-		</div>
-
-	</div>
-
+    </div>
+  </>
 );
 
 export default App;
